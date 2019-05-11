f295a9cdc3df827fc9b22d46320050465365d6f5
NathanielJenkins <njboale@gmail.com>
2019-05-10 20:21:44 -0700

    server is set up

--- app/routes/approutes.js
+++ app/routes/approutes.js
@@ -1,5 +1,17 @@
 
-var express = require("express");
-var router = express.Router();
+// express related modules
+const express = require("express");
+const router = express.Router();
 
+//database related modules
+const query = require('../tools/query');
+
+/**
+ * Test route
+ */
+router.all('/test_route', async (req, res) => {
+    return res.status(200).send('Hello Wor... *cough*.. Hello Jofi :)')
+})
+
+//export the router
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const env = require('dotenv').config(); 
 const express = require('express');
-const query = require('./app/tools/query');
-query.querydb('text', 'values')
 app = express()
 
-port = process.env.PORT;    
+//add the routes
 
+app.use("/", require('./app/routes/approutes'));
+
+//run the server
+port = process.env.PORT;    
 app.listen(port);
 console.log("starting the server on port: " + port)
-
-
